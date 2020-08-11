ff32049d3c736707f9a9701b80d6bd0bc7a075c8
yongda1028 <yundayoung1028@gmail.com>
2020-08-11 11:19:57 +0900

    2020-08-11 [automate commit check] date compare ORM

--- community/views.py
+++ community/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from community.models import Profile, Feed
+import datetime
+
+today = datetime.date.today()
 
 
-# 매일 정각에 커밋 여부를 확인하고 Profile에 대한 피드를 생성
-# feed_list = Feed.objects.filter(profile=profile)
+# 매일 커밋 여부를 확인하고 Profile에 대한 피드를 생성
 def daily_commit(request):
     profile = Profile.objects.get(profile_name='developer_yun')
+    feed_list = Feed.objects.filter(profile=profile, created_date__date=today)
 
-    return render(request, 'community/daily_commit.html')
+    return render(request, 'community/daily_commit.html', {'feed_list': feed_list})
